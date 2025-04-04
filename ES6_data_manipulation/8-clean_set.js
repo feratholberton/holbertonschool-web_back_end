a77af553d41bc4e16516e5a20907c18224401280
Fer <fernando.falcon@holbertonstudents.com>
2025-04-04 09:22:04 -0300

    Export func

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,7 @@
-function cleanSet(set, startString) {
+export default function cleanSet(set, startString) {
   if (typeof startString !== 'string' || startString.length === 0) return '';
   const filteredValues = [...set]
     .filter((value) => typeof value === 'string' && value.startsWith(startString))
     .map((value) => value.slice(startString.length));
   return filteredValues.join('-');
 }
-
-export default cleanSet;
