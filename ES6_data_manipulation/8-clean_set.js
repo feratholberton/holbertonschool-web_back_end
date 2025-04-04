a06eb85ebf2f45999b000b88b15e000b92a6fd47
Fer <fernando.falcon@holbertonstudents.com>
2025-04-04 09:16:10 -0300

    8. Clean set

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,9 @@
+function cleanSet(set, startString) {
+  if (typeof startString !== 'string' || startString.length === 0) return '';
+  const filteredValues = [...set]
+    .filter((value) => typeof value === 'string' && value.startsWith(startString))
+    .map((value) => value.slice(startString.length));
+  return filteredValues.join('-');
+}
+
+export default cleanSet;
