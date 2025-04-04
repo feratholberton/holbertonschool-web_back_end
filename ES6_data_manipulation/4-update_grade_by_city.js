a7bd47b2d403ff5d160d86c3ba9871987b27bb2e
Fer <fernando.falcon@holbertonstudents.com>
2025-04-04 08:47:04 -0300

    ()

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -5,7 +5,7 @@ export default function updateStudentGradeByCity(list, city, newGrades) {
   return list
     .filter((student) => student.location === city)
     .map(student => {
-      const gradeObj = newGrades.find(grade => grade.studentId === student.id);
+      const gradeObj = newGrades.find((grade) => grade.studentId === student.id);
       return {
         ...student,
         grade: gradeObj ? gradeObj.grade : 'N/A',
