08cb6b9aaf1347d96f48d6ee465dfcb8af33630d
Fer <fernando.falcon@holbertonstudents.com>
2025-04-04 08:28:20 -0300

    2. Filter

---
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,9 @@
+function getStudentsByLocation(list, city) {
+  if (!Array.isArray(list)) {
+    return [];
+  }
+
+  return list.filter(student => student.location === city);
+}
+
+export default getStudentsByLocation;
