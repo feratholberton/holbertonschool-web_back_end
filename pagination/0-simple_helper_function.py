cc57b8b0917463aec8490d80ee4883b7e8425650
Fer <fer.falcon.dg@gmail.com>
2025-08-13 10:53:46 -0300

    removes trailing whitespace

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -6,8 +6,8 @@ Function index_range, takes two integer arguments page and page_size
 
 def index_range(page: int, page_size: int) -> tuple:
     """
-    Return a tuple of size two containing a start index and an end index 
-    corresponding to the range of indexes to return in a list for those 
+    Return a tuple of size two containing a start index and an end index
+    corresponding to the range of indexes to return in a list for those
     particular pagination parameters.
     """
     start_index = (page - 1) * page_size
