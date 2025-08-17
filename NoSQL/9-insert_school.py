12480c822dbce53c2f2a3c1ea4ad5dc4d8921518
Fer <fer.falcon.dg@gmail.com>
2025-08-17 15:59:45 -0300

    10. Change school topics

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -6,6 +6,6 @@ from pymongo import MongoClient
 
 def insert_school(mongo_collection, **kwargs):
     """Inserts a new document in a collection based on kwargs and returns the new _id"""
-    
+
     result = mongo_collection.insert_one(kwargs)
     return result.inserted_id
