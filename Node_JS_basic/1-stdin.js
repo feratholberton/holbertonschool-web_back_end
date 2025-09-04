efac35a2edc8a98eac8771608068a5f31f4833b7
Fer <fer.falcon.dg@gmail.com>
2025-09-03 23:38:20 -0300

    1. Using Process stdin

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,13 @@
+// process.stdout.write('Xaraxa');
+console.log('Welcome to Holberton School, what is your name?')
+
+process.stdin.on('data', (data) => {
+  const input = data.toString().trim();
+  console.log(`Your name is: ${input}`);
+  process.exit();
+});
+
+process.stdin.on('end', () => {
+  console.log('This important software is now closing');
+  process.exit();
+});
--- Node_JS_basic/README.md
+++ Node_JS_basic/README.md
@@ -1,1 +1,17 @@
 # NodeJS Basics
+
+## Node has 2 module systems
+- CommonJS
+```
+require('filePath');
+module.exports = exportName;
+```
+
+- ES Modules
+```
+import name from 'filePath';
+export name;
+```
+
+
+### 0. Executing basic javascript with Node JS
