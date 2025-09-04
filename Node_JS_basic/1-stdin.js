8bdc9c71c6b5078a74b6ca27a6d1c3f96b875129
Fer <fer.falcon.dg@gmail.com>
2025-09-03 23:44:46 -0300

    No exit on data

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -2,9 +2,9 @@
 console.log('Welcome to Holberton School, what is your name?')
 
 process.stdin.on('data', (data) => {
-  const input = data.toString().trim();
+  const input = data.toString();
   console.log(`Your name is: ${input}`);
-  process.exit();
+  //process.exit();
 });
 
 process.stdin.on('end', () => {
