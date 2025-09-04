5c3e66999d493f0c7ca44a904e9da9f024b85a09
Fer <fer.falcon.dg@gmail.com>
2025-09-03 23:49:58 -0300

    Test

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,13 +1,14 @@
-// process.stdout.write('Xaraxa');
-console.log('Welcome to Holberton School, what is your name?')
+process.stdin.setEncoding('utf8');
 
-process.stdin.on('data', (data) => {
-  const input = data.toString();
-  console.log(`Your name is: ${input}`);
-  //process.exit();
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name !== null) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
 });
 
 process.stdin.on('end', () => {
-  console.log('This important software is now closing');
-  process.exit();
+  process.stdout.write('This important software is now closing\n');
 });
