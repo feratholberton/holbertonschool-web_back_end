c1ea7945f1ae3edb8a01b9149b4b4c0cc0d79e4c
Fer <fer.falcon.dg@gmail.com>
2025-09-06 17:30:19 -0300

    5. Create a more complex HTTP server using Node's HTTP module

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -5,14 +5,16 @@ const hostname = 'localhost';
 const port = 1245;
 
 const app = createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-
+  
   if (req.url === '/') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
     res.end('Hello Holberton School!');
   }
 
   if (req.url === '/students') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
     const students = countStudents(process.argv[2]);
     res.end(students);
   }
