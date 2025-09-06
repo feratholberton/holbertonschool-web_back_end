867cab833dd875539f383a95e498e59558b11c82
Fer <fer.falcon.dg@gmail.com>
2025-09-06 14:09:47 -0300

    Trailing spaces

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -6,16 +6,16 @@ function countStudents(path) {
     const totalLines = data.split('\n');
     const usefulLines = totalLines.filter((line) => line.trim() !== '');
     const studentsLines = usefulLines.splice(1);
-    
+
     const students = [];
     studentsLines.forEach((student) => students.push(student.split(',')));
 
     console.log(`Number of students: ${students.length}`);
-    
+
     const fields = [];
     students.forEach((field) => fields.push(field[3]));
     const uniqueFields = (new Set(fields));
-    
+
     uniqueFields.forEach((value) => {
       const studentInField = [];
       students.forEach((student) => {
