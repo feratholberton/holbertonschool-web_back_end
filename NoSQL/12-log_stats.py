4107c8a610a27e1131dc22182aa93c96959d5aa6
Fer <fer.falcon.dg@gmail.com>
2025-11-08 09:50:22 -0300

    12. Log stats

---
+++ NoSQL/12-log_stats.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""Provide simple stats about Nginx logs stored in MongoDB."""
+
+from pymongo import MongoClient
+
+
+def log_stats():
+    """Prints log statistics for the logs.nginx collection."""
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    collection = client.logs.nginx
+
+    total_logs = collection.count_documents({})
+    print(f"{total_logs} logs")
+
+    print("Methods:")
+    for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
+        count = collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {count}")
+
+    status_check = collection.count_documents({"method": "GET", "path": "/status"})
+    print(f"{status_check} status check")
+
+
+if __name__ == "__main__":
+    log_stats()
